6d1ea4f494cef90321c93998dc532b6118b5f278
jessikim <jessica.kim17@gmail.com>
2022-11-30 17:15:52 -0600

    update potluck schema and add rsvp schema that will be embedded

--- models/potluck.js
+++ models/potluck.js
@@ -2,14 +2,64 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
-const potluckSchema = new Schema({
-  name: String,
-  location: String,
-  description: String,
-  host: { type: Schema.Types.ObjectId, ref: 'Profile' }
-}, {
-  timestamps: true
-})
+const RSVPSchema = new Schema(
+  {
+    guest: {
+      type: Schema.Types.ObjectId, ref: 'Profile'
+    },
+    party: {
+      type: Number,
+      required: true
+    },
+    notes: {
+      type: String
+    },
+    rsvp: {
+      type: String,
+      required: true,
+      enum: ['Yes', 'No', 'Maybe']
+    }
+  },
+  { timestamps: true }
+)
+
+const potluckSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    start: {
+      type: Date,
+      required: true
+    },
+    end: {
+      type: Date,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String
+    },
+    host: { 
+      type: Schema.Types.ObjectId, ref: 'Profile' 
+    },
+    rsvps: [RSVPSchema],
+    food: { 
+      type: Schema.Types.ObjectId, ref: 'Food' 
+    },
+    items: { 
+      type: Schema.Types.ObjectId, ref: 'Item' 
+    },
+    drinks: { 
+      type: Schema.Types.ObjectId, ref: 'Drink' 
+    },
+  }, 
+  { timestamps: true }
+)
 
 const Potluck = mongoose.model('Potluck', potluckSchema)
 
